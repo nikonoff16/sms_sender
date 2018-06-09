ea3e3832f6570ecef1d24783aad5fc21f02bdd56
Victor Osipov <i.nikonoff16@gmail.com>
2018-06-09 21:45:14 +0500

    Third

--- kalendar.py
+++ kalendar.py
@@ -5,3 +5,6 @@ kalendar = {'Date': ('preacher1', 'preacher2')}
 
 preachers_list = {'preacher1':('89997776655', '88884443322'),
                   'preacher2': ('80001112233')}
+
+
+# You shall not see this
