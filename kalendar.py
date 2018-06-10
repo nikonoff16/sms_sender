3d7da42161225302f1cac5a9328617896fa31335
Victor Osipov <37671082+nikonoff16@users.noreply.github.com>
2018-06-10 23:33:50 +0500

    Почти работает
    Смотри README

--- kalendar.py
+++ kalendar.py
@@ -3,8 +3,8 @@
 
 events_list = {(2018, 6, 17): ('Victor Osipov', 'Katya Osipova')}
 
-preachers_list = {'Victor Osipov':(''),
-                  'Katya Osipova': ('')}
+preachers_list = {'Victor Osipov':('+79129760922'),
+                  'Katya Osipova': ('+79965574689')}
 
 text = "Try to send text in Russian"
 # You shall not see this'
