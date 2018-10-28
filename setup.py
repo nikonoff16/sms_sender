6d5330811215e78578f4d66793e07d4bba93c8bf
Viktor Osipov <i.nikonoff16@gmail.com>
2018-10-28 23:02:04 +0500

    Starting to create setup file

--- setup.py
+++ setup.py
@@ -3,25 +3,60 @@
 
 '''
 Module manifest
-The module should interact with *.json files in the program and provide for that humanreadable 
+The module should interact with *.json files in the program and provide for that human-frendly 
 interface. 
-The core functionality is to change or create events base and preachers base (events_base and
-preachers_base accordingly in the code) through one console window. 
+The core functionality: change or create events_base and preachers_base files through one console window. 
 
 Bussiness logic:
 - Show current state of events_base and preachers_base
 - Ask is it right that user wants to setup the program
-- If user acsepts the request, ask him what time period he going to setup (by default it shall be 92 days 
+- If user acsepts the request, ask him what time period he is going to setup (by default it shall be 92 days 
   and starts from last date in event_base)
 - The scrit starts for cycle and parses every date in that period. 
-- Translate it next time you will be here: 	• Обработчики - большая конструкция из if-elif-…-else, которая отрабатывает различные правила событий.
-	• Правила могут относиться ко дню недели (сб, вс), дню месяца (25.12, 07.01), или конкретному календарному событию (прописать список таковых на лет 50 вперед, или формулу их вписать в правило). 
-		○ С каждом правиле есть указатель темы. 
-	• В правиле для создания события нужно использовать функцию create_event()
-	• Если правило события, то открывается форма создания события, в которой пользователь заполняет: 
-		○ Проповедники
-		○ Ведущие собрания
-		○ Рекомендуемый текст
-	• По завершению в итерации - создание json-файла из словаря, в который аугментируются созданные события. (Чтобы не потерять результаты в случае сбоя программы на очередном этапе.)
-По завершении цикла создать копию списка в отдельную папку и удалить те значения в списке, которые стоят высше текущей даты. 
+- The processors - the big il-elif-else construction - shall manage different event rules.
+- The rules may relate to the weekday, day of the month (25.12, 07.01) or any other calculated church event, like Pascha.
+	- Every rule shall have its own theme marker.
+- Every rule shall use create_event() function. 
+- Every event shall have following points:
+	- Preachers
+	- Leaders
+	- Proposal text
+- When an iteration ends, script shall update json-file with new data.
+- When the cycle ends, script shall
+	- create a copy of json-file and place it in designated directory
+	- delete all the events, that is older then current date
 '''
+import json 
+import datetime
+import time
+
+# Creating global variables
+try:
+	with open("preachers_base.json", "r") as read_file:
+		preachers_list = json.load(read_file)
+except FileNotFoundError:
+	preachers_list = {}
+
+try:
+	with open("events_base.json", "r") as read_file:
+		events_base = json.load(read_file)
+except FileNotFoundError:
+	events_base = {}
+
+try:
+	with open("api.json", "r") as api_file:
+		api = json.load(api_file)
+except FileNotFoundError:
+	api = {"login": 0, "myapi": 0}
+
+# Creating date-variables 
+# I don't know for sure what date standard I would use in the main cycle, so let it be both there.s	
+
+date_cur_date = datetime.datetime.now()
+str_cur_date = str(date_cur_date)[:10]  # I need only YYYY-MM-DD date format, so by slicing to 10 I make it
+
+
+
+print(date_cur_date, str_cur_date)
+
+
